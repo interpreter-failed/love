5641a517ac84bf6c95a9d5ca0567195d462fee8d
David Landa <david.landa@protonmail.com>
2022-03-26 14:01:07 +0100

    Add scripts and letters folder

---
+++ scripts/__init__.py
@@ -0,0 +1,33 @@
+"""
+I love when you f**k me rough and hard.
+"""
+
+
+import sys
+import math
+import datetime as dt
+
+
+__all__ = tuple(["hours_to_meeting"])
+
+
+def hours_to_meeting(finish: dt.date, start: dt.date = dt.datetime.now()):
+    return abs(finish - start).total_seconds() / 3600.0
+
+
+def main() -> None:
+
+    match len(sys.argv):
+        case 2: # one date
+            finish = dt.datetime.fromisoformat(sys.argv[1])
+            hours = hours_to_meeting(finish)
+            print(f"We will meet after {math.floor(hours)} hours.")
+        case 3: # two dates
+            print(sys.argv[1], sys.argv[2])
+            # TODO
+        case _:
+            print("Pick a date and time in the future e.g. '2022-03-28T21:30:00'")
+            # raise Exception("Bad input.")
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import setuptools as st
+
+st.setup(
+    name="next_meet",
+    version="0.2.0",
+    install_requires=[
+        #
+    ]
+)
