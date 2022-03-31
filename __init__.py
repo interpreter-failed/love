9df7a7c7d72989d83c11d0122bc35323c9fd8e4f
David Landa <david.landa@protonmail.com>
2022-03-31 09:46:42 +0200

    Add basic encryption/decryption functionality

--- __init__.py
+++ __init__.py
@@ -1,23 +1,35 @@
+# -*- coding: utf-8 -*-
+
 """
 I love when you f**k me rough and hard.
 """
 
-
 import sys
 import math
 import datetime as dt
+from typing import IO, TextIO, BinaryIO
+
+from cryptography.fernet import Fernet
 
 
 __all__ = tuple(["hours_to_meeting"])
 
 
-def encrypt_file(file) -> 'encrypted_file':
-    ...
+# ############################################################################
+# Message encryption and decryption with symmetric key.
+# ############################################################################
+
+def encrypt_message(key: bytes, message: BinaryIO) -> bytes:
+    return Fernet(key).encrypt(message)
 
 
-def decrypt_file(file) -> 'decrypted_file':
-    ...
+def decrypt_message(key: bytes, message: BinaryIO) -> bytes:
+    return Fernet(key).decrypt(message)
 
+
+# ############################################################################
+# Meeting date/time calculations
+# ############################################################################
 
 def hours_to_meeting(finish: dt.date, start: dt.date = dt.datetime.now()) -> float:
     """
@@ -25,6 +37,8 @@ def hours_to_meeting(finish: dt.date, start: dt.date = dt.datetime.now()) -> flo
     """
     return abs(finish - start).total_seconds() / 3600.0
 
+
+# ############################################################################
 
 def main() -> None:
 
@@ -40,6 +54,20 @@ def main() -> None:
             print("Pick a date and time in the future e.g. '2022-03-28T21:30:00'")
             # raise Exception("Bad input.")
 
+# ############################################################################
 
 if __name__ == "__main__":
-    main()
+
+    # This is only a demo.
+    with open("./letters/test.md", encoding="utf-8") as file:
+
+        key = Fernet.generate_key()
+        print(f"KEY: {key}")
+
+        encrypted = encrypt_message(key, str.encode(file.read()))
+        print(f"ENCRYPTED: {encrypted}")
+
+        decrypted = decrypt_message(key, encrypted).decode()
+        print(f"DECRYPTED: {decrypted}")
+
+    # main()
